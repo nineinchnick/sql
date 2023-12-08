5ab779c68f519bb7a6e94df4977443903c9e3a50
Jan Waś <jan.was@starburstdata.com>
2023-12-08 17:20:55 +0100

    Add a better ascii bar and vertical bar example

--- trino/vertical_bar.sql
+++ trino/vertical_bar.sql
@@ -7,36 +7,61 @@ CREATE OR REPLACE FUNCTION vertical_bar(value DOUBLE)
     RETURN ARRAY[' ', '▁', '▂', '▃', '▄', '▅', '▆', '▇', '█'][cast(value * 8 + 1 as int)];
 
 WITH
-data AS (
-    SELECT s.num / 10.0 AS num
-    FROM table(sequence(start=>0, stop=>10, step=>1)) AS s(num)
+measurements(sensor_id, recorded_at, value) AS (
+    VALUES
+        ('A', date '2023-01-01', 5.0)
+      , ('A', date '2023-01-03', 7.0)
+      , ('A', date '2023-01-04', 15.0)
+      , ('A', date '2023-01-05', 14.0)
+      , ('A', date '2023-01-08', 10.0)
+      , ('A', date '2023-01-09', 1.0)
+      , ('A', date '2023-01-10', 7.0)
+      , ('A', date '2023-01-11', 8.0)
+      , ('B', date '2023-01-03', 2.0)
+      , ('B', date '2023-01-04', 3.0)
+      , ('B', date '2023-01-05', 2.5)
+      , ('B', date '2023-01-07', 2.75)
+      , ('B', date '2023-01-09', 4.0)
+      , ('B', date '2023-01-10', 1.5)
+      , ('B', date '2023-01-11', 1.0)
+)
+, days AS (
+    SELECT date_add('day', s.num, date '2023-01-01') AS day
+    -- table function arguments need to be constant but range could be calculated
+    -- using: SELECT date_diff('day', max(recorded_at), min(recorded_at)) FROM measurements
+    FROM table(sequence(start=>0, stop=>10)) AS s(num)
+)
+, sensors(id) AS (VALUES ('A'), ('B'))
+, normalized AS (
+    SELECT
+        sensors.id AS sensor_id
+      , days.day
+      , value
+      , value / max(value) OVER (PARTITION BY sensor_id) AS normalized
+    FROM days
+    CROSS JOIN sensors
+    LEFT JOIN measurements m ON day = recorded_at AND m.sensor_id = sensors.id
 )
 SELECT
-    data.num
-  , vertical_bar(data.num) AS chart
-FROM data
-UNION ALL
-SELECT
-    11
-  , (SELECT array_join(array_agg(vertical_bar(num) ORDER BY num), '') FROM data)
-ORDER BY num;
+      sensor_id
+    , min(day) AS start
+    , max(day) AS stop
+    , count(value) AS num_values
+    , min(value) AS min_value
+    , max(value) AS max_value
+    , avg(value) AS avg_value
+    , array_join(array_agg(coalesce(vertical_bar(normalized), ' ') ORDER BY day), '') AS distribution
+FROM normalized
+WHERE sensor_id IS NOT NULL
+GROUP BY sensor_id
+ORDER BY sensor_id;
 
 /* expected output:
 CREATE FUNCTION
- num  |    chart
-------+-------------
-  0.0 |
-  0.1 | ▁
-  0.2 | ▂
-  0.3 | ▂
-  0.4 | ▃
-  0.5 | ▄
-  0.6 | ▅
-  0.7 | ▆
-  0.8 | ▆
-  0.9 | ▇
-  1.0 | █
- 11.0 |  ▁▂▂▃▄▅▆▆▇█
-(12 rows)
+ sensor_id |   start    |    stop    | num_values | min_value | max_value | avg_value | distribution
+-----------+------------+------------+------------+-----------+-----------+-----------+--------------
+ A         | 2023-01-01 | 2023-01-11 |          8 |      1.00 |     15.00 |      8.38 | ▃ ▄█▇  ▅▁▄▄
+ B         | 2023-01-01 | 2023-01-11 |          7 |      1.00 |      4.00 |      2.39 |   ▄▆▅ ▆ █▃▂
+(2 rows)
 */
 
